68bd41aefe4cea02499157e1c149b7059fbc8aa9
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-02 19:01:32 +0530

    added boarder on logo also some changes for navigation

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white shadow-md w-full fixed top-0 left-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
           <Link
