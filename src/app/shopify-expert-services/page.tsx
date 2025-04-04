e2d3bad94dc41b6f6c7019a42f84ec2047cb2165
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-04 11:03:37 +0530

    pricing page added

--- src/app/shopify-expert-services/page.tsx
+++ src/app/shopify-expert-services/page.tsx
@@ -10,15 +10,8 @@ export default function pricing() {
   return (
     <main className="container mx-auto px-4 py-10 text-center pt-20">
         <h1 className="text-3xl md:text-6xl font-regular">Shopify Expert Services</h1>
-        <h2 className="text-3xl md:text-4xl font-regular">If you're ready to design, develop or scale on Shopify we have the solution.
-        </h2>
-    <p>Commercial Shopify themes & apps are great for lower level merchants.  But not for big brands.  You cant share a design with another company by using a template.  You just can’t.  You need a custom build, created around your brand.  Each and every pixel needs to be yours.  Design.  Development.  100% unique.
-
-At Liquify we specialise in building custom Shopify themes from scratch as well as custom apps.  We build compete bespoke ecommerce experiences around brands and very specific merchant needs.
-
-Custom builds take considerable time and effort and projects typically last for 2-5 months depending on the complexity.  Cost bracket of $20k-120k.
-
-Interested to know what we can do to help?  Use the form below to start the conversation.</p>
+       
+    <p>Commercial Shopify themes </p>
     </main>
   );
 }
