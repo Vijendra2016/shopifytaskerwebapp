b545a87374810e99d729061591285ce580bc28bb
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-04 11:02:06 +0530

    pricing page added

--- src/app/shopify-expert-services/page.tsx
+++ src/app/shopify-expert-services/page.tsx
@@ -12,7 +12,7 @@ export default function pricing() {
         <h1 className="text-3xl md:text-6xl font-regular">Shopify Expert Services</h1>
         <h2 className="text-3xl md:text-4xl font-regular">If you're ready to design, develop or scale on Shopify we have the solution.
         </h2>
-    <p>Commercial Shopify themes & apps are great for lower level merchants.  But not for big brands.  You can’t share a design with another company by using a template.  You just can’t.  You need a custom build, created around your brand.  Each and every pixel needs to be yours.  Design.  Development.  100% unique.
+    <p>Commercial Shopify themes & apps are great for lower level merchants.  But not for big brands.  You cant share a design with another company by using a template.  You just can’t.  You need a custom build, created around your brand.  Each and every pixel needs to be yours.  Design.  Development.  100% unique.
 
 At Liquify we specialise in building custom Shopify themes from scratch as well as custom apps.  We build compete bespoke ecommerce experiences around brands and very specific merchant needs.
 
