36fa0a2122d64bce2498ed8d7c01641d9a6f0dc5
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-13 22:50:01 +0530

    change footer menu size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,10 +103,9 @@ export default function Home() {
 <div className="services flex flex-col items-center justify-center text-center text-black px-6 py-16 space-y-6 max-w-5xl mx-auto">
   <h3 className="text-3xl md:text-6xl font-regular ">
     How its work</h3>
-  <p >
-  It's simple — just submit your task, and within 5 hours, you will hear back from us with a detailed quote. If we need any additional information, we will arrange a quick discovery call to better understand your requirements.
+  
+    <p>It is simple — just submit your task, and within 5 hours, you will hear back from us with a detailed quote. If we need any additional information, we will arrange a quick discovery call to understand your requirements better.</p>
 
-</p>
   </div>
 </section>
     
