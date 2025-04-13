ec3c74345177ff05abb66ec018ad8ebc0f54f6ea
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-13 18:34:22 +0530

    color schema chnage

--- src/app/shopify-small-task/page.tsx
+++ src/app/shopify-small-task/page.tsx
@@ -5,15 +5,12 @@ export const metadata: Metadata = {
     "shopify experts small tasks",
 };
 
-export default function pricing() {
+export default function smallTask() {
   return (
     <main className="container mx-auto px-4 py-10 text-center pt-20">
         <h1 className="text-3xl md:text-6xl font-regular">We are shopify experts small tasks    </h1>
         
     <p>We are top rate shopify developer based in udaipur rajsthan</p>
-</p>
-
-
 
 
 
