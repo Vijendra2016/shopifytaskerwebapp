b74d7b688c4266b75c0ebf7a5fdfa57eec70f9c7
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-13 18:32:58 +0530

    color schema chnage

--- src/app/shopify-small-task/page.tsx
+++ src/app/shopify-small-task/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import Image from "next/image";
 export const metadata: Metadata = {
   title: "shopify small tasks by shopify expert | Top Rated Developer",
   description:
@@ -10,9 +9,8 @@ export default function pricing() {
   return (
     <main className="container mx-auto px-4 py-10 text-center pt-20">
         <h1 className="text-3xl md:text-6xl font-regular">We are shopify experts small tasks    </h1>
-        <h2 className="text-3xl md:text-4xl font-regular">If youre ready to design, develop or scale on Shopify we have the solution.
-        </h2>
-    <p>Commercial Shopify themes & apps are great for lower level merchants.  But not for big brands.  You can’t share a design with another company by using a template.  You just can’t.  You need a custom build, created around your brand.  Each and every pixel needs to be yours.  Design.  Development.  100% unique.
+        
+    <p>We are top rate shopify developer based in udaipur rajsthan</p>
 </p>
 
 
