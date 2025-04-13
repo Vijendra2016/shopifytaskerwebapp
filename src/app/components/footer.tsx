afc86197c184cd2aea0d1d873ede75c8cd2a2325
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-13 22:12:56 +0530

    change footer menu size

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#f8f8f8] text-lg text-black py-12 px-6">
+    <footer className="bg-[#f8f8f8] text-sm text-black py-12 px-6">
       
 
       <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
@@ -20,7 +20,7 @@ export default function Footer() {
         {/* Column 1 */}
         <div className="flex flex-col space-y-2">
         
-          <h6 className="underline text-xl md:text-1xl font-thin">About</h6>
+          <h6 className="underline text-base md:text-1xl font-thin">About</h6>
           <Link href="/" className="underline">about us</Link>
           <Link href="/how-it-works">how it works</Link>
           <Link href="/experts">our expert</Link>
@@ -31,7 +31,7 @@ export default function Footer() {
 
         {/* Column 2 */}
         <div className="flex flex-col space-y-2">
-          <h6 className=" underline text-xl md:text-1xl font-thin">Our Services</h6>
+          <h6 className=" underline text-base md:text-1xl font-thin">Our Services</h6>
           <Link href="#">store setup and development</Link>
           <Link href="/partner">Platform migration</Link>
           <Link href="/start-project">Custom app development</Link>
@@ -44,7 +44,7 @@ export default function Footer() {
 
         {/* Column 3 - Duplicate of Column 2 (you may want to change heading/content) */}
         <div className="flex flex-col space-y-2">
-          <h6 className="underline text-xl md:text-1xl font-thin">Small Task</h6>
+          <h6 className="underline text-base md:text-1xl font-thin">Small Task</h6>
           <Link href="/become-expert">Shopify Theme tweak</Link>
           <Link href="/partner">Shopify App Development</Link>
           <Link href="/start-project">Shopify Hydrogen</Link>
@@ -55,7 +55,7 @@ export default function Footer() {
         </div>
          {/* Column 3 - Duplicate of Column 2 (you may want to change heading/content) */}
          <div className="flex flex-col space-y-2">
-          <h6 className=" underline text-3xl text-xl font-thin">Headless Shopify Development</h6>
+          <h6 className=" underline text-base font-thin">Headless Shopify Development</h6>
           <Link href="/become-expert">Shopify Hyrogen Store Setup</Link>
           <Link href="/partner">Shopify Headless Store Managment</Link>
           <Link href="/start-project">start a project</Link>
@@ -65,7 +65,7 @@ export default function Footer() {
           <Link href="/customer-login">customer login</Link>
         </div>
         <div className="flex flex-col space-y-2">
-          <h6 className=" underline text-3xl text-xl font-thin">Headless Shopify Development</h6>
+          <h6 className=" underline text-base font-thin">Headless Shopify Development</h6>
           <Link href="/become-expert">Shopify Hyrogen Store Setup</Link>
           <Link href="/partner">Shopify Headless Store Managment</Link>
           <Link href="/start-project">start a project</Link>
@@ -75,7 +75,7 @@ export default function Footer() {
           <Link href="/customer-login">customer login</Link>
         </div>
         <div className="flex flex-col space-y-2">
-          <h6 className=" underline text-3xl text-xl font-thin">Platform Migration</h6>
+          <h6 className=" underline text-base font-thin">Platform Migration</h6>
           <Link href="/become-expert">Wordpress to Shopify Migration</Link>
           <Link href="/partner">Salesfoce to Shopify Plus</Link>
           <Link href="/start-project">Squarespace to shopify</Link>
@@ -86,7 +86,7 @@ export default function Footer() {
         </div>
 
         <div className="flex flex-col space-y-2">
-          <h6 className=" underline text-3xl text-xl font-thin">Theme Customization & Update Service</h6>
+          <h6 className=" underline text-base font-thin">Theme Customization & Update Service</h6>
           <Link href="/become-expert">Impact theme Customization & Update</Link>
           <Link href="/partner">Shopify Headless Store Managment</Link>
           <Link href="/start-project">start a project</Link>
