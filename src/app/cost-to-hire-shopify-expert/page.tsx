60f4e1fcbc4cded7f9c400c4c021b34332a297af
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-16 08:36:17 +0530

    added pricing

--- src/app/cost-to-hire-shopify-expert/page.tsx
+++ src/app/cost-to-hire-shopify-expert/page.tsx
@@ -10,9 +10,10 @@ export default function pricing() {
   return (
     <main className="container mx-auto px-4 py-10 text-center pt-20">
 
-        <h1 className="text-3xl md:text-6xl font-regular">Fair prices No Hidden Fees</h1>
-        <h2 className="text-3xl md:text-4xl font-regular">100% Transparency, No BS, No dodgy stuff. Just pure results.</h2>
-    
+        <h1 className="text-3xl md:text-6xl font-regular">Honest Pricing for Every Task & project </h1>
+        <p>Consistent and predictable rates across our team of in-house Shopify experts. Whether it a quick fix or a full-scale project, you always get transparent, reliable pricing. Request free quotes anytime or choose pre-paid hourly bundles to save and stay in control of your budget.
+
+</p>
     </main>
   );
 }
