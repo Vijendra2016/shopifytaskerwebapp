d3c7a59b6d570376f095063e4aa7c43835e190ac
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-17 12:41:04 +0530

    new reviews and review page update

--- src/app/components/TestimonialCardGrid.tsx
+++ src/app/components/TestimonialCardGrid.tsx
@@ -43,7 +43,7 @@ const testimonials = [
 
 const TestimonialCardGrid = () => {
   return (
-    <div className="bg-[#f3f3f1] py-12 px-4 md:px-8">
+    <div className="bg-[#c8ddb8] py-12 px-4 md:px-8">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {testimonials.map((testimonial, index) => (
           <div
