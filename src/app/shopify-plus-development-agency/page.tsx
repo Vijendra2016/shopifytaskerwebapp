04f7a4f24d115e3e33f6650f3e2a665a524c433a
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-22 22:39:55 +0530

    added message on location based

---
+++ src/app/shopify-plus-development-agency/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Shopify Plus Development Agency in USA |Top Rated shopify-plus-developers",
+  description:
+    "Looking shopify plus development agency in USA?",
+};
+
+export default function smallTask() {
+  return (
+    <main className="container mx-auto px-4 py-10 text-center pt-20">
+        <h1>shopify plus development agency </h1>
+       
+    <p>We can help you with shopify plus development , are you looking for shopify plus developer for your store development or mantaince    </p>
+
+
+
+    </main>
+  );
+}
+
