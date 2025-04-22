a45d08254b6834df4c47032137fb55b2f6230538
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-22 10:16:56 +0530

    newyork city added

---
+++ src/app/shopify-developer-near-me/newyork/page.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+import TestimonialCardGrid from "@/app/components/TestimonialCardGrid";
+export const metadata: Metadata = {
+  title: "Shopify Developer Near Me new york| shopify developer in Newyork",
+  description:
+    "Looking to hire shopify developer in new york ? we can help you with shopify development on very affordable prices.",
+};
+
+export default function pricing() {
+  return (
+    <main className="container mx-auto px-4 py-10 text-center pt-20">
+        <h1 className="text-3xl md:text-6xl font-regular">Looking to hire shopify developer in  new york       </h1>
+      
+          <p className="text-gray-600 text-lg">
+          ShopifyTasker is highly praised for its reliable and efficient services, offering expert help with store setup,
+           optimization, and marketing. Users appreciate its quick turnaround and excellent customer support
+
+        </p>
+<TestimonialCardGrid/>
+
+    </main>
+  );
+}
