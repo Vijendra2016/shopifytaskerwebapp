01a05a070d8491154a3329ae02ca4dd43c3322a7
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-25 16:44:41 +0530

    added message change

--- src/app/components/LocationMessage.tsx
+++ src/app/components/LocationMessage.tsx
@@ -29,12 +29,12 @@ const LocationMessage = () => {
     switch (country) {
       case "US":
         return city
-          ? `Looking Shopify developer & expert near ${city}, U.S.? Local Shopify developers typically charge $80 t0 $120/hour — our experts deliver the same quality from just $25/hour, and you pay only after delivery Trusted by leading Australian brands · 100+ projects delivered · Pay later with confidence`
+          ? `Looking Shopify developer & expert near ${city}, U.S.? Local Shopify developers typically charge $80 t0 $120/hour — our experts deliver the same quality from just $25/hour, and you pay only after delivery`
           : "Looking Shopify developer & expert in the U.S.? Local Shopify  developers typically charge $80 t0 $120/hour — our experts deliver the same quality from just $25/hour, and you pay only after delivery!";
   
       case "AU":
         return city
-          ? `Looking Shopify developer & expert near ${city}, Australia? While local developers often charge $100+/hour, our experienced team offers the same premium quality for just $35 AUD/hour.`
+          ? `Looking Shopify developer & expert near ${city}, Australia? While local developers often charge $100+/hour, our experienced team offers the same premium quality for just $35 AUD/hour.Trusted by leading Australian brands · 100+ projects delivered · Pay later with confidence`
           : "Looking Shopify developer & expert in Australia? Local devs cost $100+/hour — get the same quality from us at $35 AUD/hour, pay later!";
   
       case "IN":
