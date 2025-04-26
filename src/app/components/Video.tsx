628af89db47f8cd8f5dff2dc74b8c54d31e98f0d
Vjndevshopify <vijendra.singh@retrospec.com>
2025-04-26 12:14:25 +0530

    added video section

--- src/app/components/Video.tsx
+++ src/app/components/Video.tsx
@@ -7,7 +7,7 @@ const VideoPlayer = () => {
       <iframe
         loading="lazy"
         title="Gumlet video player"
-        src="https://play.gumlet.io/embed/680c6ff80d7002c9e68a7e10?preload=true&autoplay=true&loop=true&background=true&disable_player_controls=true"
+        src="https://play.gumlet.io/embed/680c8076258db0ded54545cb?preload=true&autoplay=true&loop=true&background=true&disable_player_controls=true"
         className="absolute top-0 left-0 w-full h-full border-none"
         allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen"
       />
