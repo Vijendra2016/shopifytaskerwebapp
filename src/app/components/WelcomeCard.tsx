5fc7fb7ef0544c8b5354d9d16bac23b4cbffe817
Vjndevshopify <vijendra.singh@retrospec.com>
2025-05-03 17:49:55 +0530

    color theory added

--- src/app/components/WelcomeCard.tsx
+++ src/app/components/WelcomeCard.tsx
@@ -17,16 +17,16 @@ export default function WelcomeCard() {
     <>
       <div
         onClick={handleCardClick}
-        className="fixed bottom-4 left-2 bg-black border shadow-lg p-4 rounded-xl z-40 max-w-sm cursor-pointer"
+        className="fixed bottom-1 righ-2 bg-black border  border-cyan-400 shadow-lg p-4 rounded-xl z-40 max-w-sm cursor-pointer"
       >
-        <p className="text-white text-lg mb-2">
+        <p className="text-cyan-400 text-lg mb-2">
         Kickstart your first project with confidence
         </p>
         <LocationMessage/>
         
         <button
           onClick={() => setShowModal(true)}
-          className=" cursor-pointer  animate-wiggle animate-infinite inline-flex items-center bg-green-300 text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-blue-400  transition"
+          className=" cursor-pointer   inline-flex items-center bg-teal-500  text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-blue-400  transition"
         >
           Get Started 
           <TfiArrowTopRight className="ml-2" />
