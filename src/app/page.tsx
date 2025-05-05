64100ac381ce108f068cc417d401817c8fb0f6ce
Vjndevshopify <vijendra.singh@retrospec.com>
2025-05-05 16:29:09 +0530

    how its working updated pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
   <main className="flex flex-col  w-full text-center sm:text-left">
     <section className="px-6 md:px-30 py-16 bg-black text-white">
     <div className="services flex flex-col items-center justify-center text-center text-black ">
-<h2 className="text-3xl md:text-9xl text-white font-medium ">Shopify Expert,on demand.</h2>
-<h3 className="text-lg md:text-5xl text-white  font-medium"> No upfront payment required </h3>
-<h4 className="text-lg md:text-4xl text-white"> <span className="bg-teal-500 text-black">Pay only after</span> your task is completed to your satisfaction. </h4>
-<h4 className="text-lg md:text-3xl text-white "> 10 Years experience in shopify ecosystem </h4>
+<h2 className="text-3xl md:text-9xl text-white font-medium tracking-tight ">Shopify Expert,on demand.</h2>
+<h3 className="text-lg md:text-5xl text-white  font-medium tracking-tight"> No upfront payment required </h3>
+<h4 className="text-lg md:text-4xl text-white tracking-tight"> <span className="bg-teal-500 text-black">Pay only after</span> your task is completed to your satisfaction. </h4>
+<h4 className="text-lg md:text-3xl text-white tracking-tight "> 10 Years experience in shopify ecosystem </h4>
 
 
 </div>
@@ -39,7 +39,7 @@ export default function Home() {
   <h3 className="text-3xl text-black md:text-6xl font-regular ">
     How its work</h3>
   
-    <p className="text-sm text-black">It is simple — just submit your task, and within 5 hours, you will hear back from us with a detailed quote. If we need any additional information, we will arrange a quick discovery call to understand your requirements better.</p>
+    <p className="text-3xl text-black tracking-tight">It is simple — just submit your task, and within 5 hours, you will hear back from us with a detailed quote. If we need any additional information, we will arrange a quick discovery call to understand your requirements better.</p>
 
   </div>
 </section>
