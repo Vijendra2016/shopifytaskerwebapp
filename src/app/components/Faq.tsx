ddba97b9d75780166676125810ae51251a395892
Vjndevshopify <vijendra.singh@retrospec.com>
2025-05-05 10:45:32 +0530

    faq page styling

--- src/app/components/Faq.tsx
+++ src/app/components/Faq.tsx
@@ -25,28 +25,28 @@ export default function FaqSections({ sections }: FaqSectionsProps) {
   };
 
   return (
-    <div className="bg-[#faebd7] max-w-4xl mx-auto p-6 space-y-10 mb-20">
+    <div className="pr-2 pl-2 lg:pr-20 lg:pl-20  md:pr-20 md:pl-20 pt-20 pb-20  bg-black text-white">
       {sections.map((section, sectionIndex) => (
         <div key={sectionIndex}>
-          <h2 className="text-2xl font-bold mb-6 ">{section.title}</h2>
+          <h2 className="text-5xl tracking-tight font-bold mb-6 pt-10 ">{section.title}</h2>
           <div className="space-y-0 cursor-pointer">
             {section.items.map((item, itemIndex) => {
               const isOpen = openQuestion === item.question;
               return (
-                <div key={itemIndex} className="border-b bg-white ">
+                <div key={itemIndex} className="border-t border-cyan-400 bg-black ">
                   <button
                     onClick={() => toggleQuestion(item.question)}
-                    className="w-full flex justify-between items-center p-4 text-left"
+                    className="w-full flex cursor-pointer justify-between items-center p-4 text-left"
                   >
-                    <span className="text-lg font-semibold cursor-pointer">{item.question}</span>
+                    <span className="text-3xl  tracking-tight text-white font-semibold cursor-pointer">{item.question}</span>
                     <TfiAngleDown
-                      className={`h-5 cursor-pointer w-5 transition-transform duration-300 ${
+                      className={`h-5 cursor-pointer text-white  w-5 transition-transform duration-300 ${
                         isOpen ? 'rotate-180' : ''
                       }`}
                     />
                   </button>
                   {isOpen && (
-                    <div className="px-4 pb-4 text-gray-600 text-left">
+                    <div className="px-4 pb-4 text-white text-lg text-left">
                       {item.answer}
                     </div>
                   )}
