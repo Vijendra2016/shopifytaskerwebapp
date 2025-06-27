eee27c1c4c9bf7f90913f87602540ab9a0e11d82
Vjndevshopify <vijendra.singh@retrospec.com>
2025-06-27 17:10:43 +0530

    prices changes for usa  to

---
+++ src/app/hire-front-end-developer-shopify/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Hire shopify front end developer  ",
+  description:
+    "hire dedicated shopify front end developer @10 per hours",
+};
+
+export default function smallTask() {
+  return (
+    <main className="container mx-auto px-4 py-10 text-center pt-20">
+        <h1>Hire shopify front end developer @10 per hours  </h1>
+       
+    <p>We can help you with shopify development company india </p>
+
+
+    </main>
+  );
+}
+
