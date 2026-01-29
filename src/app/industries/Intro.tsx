23b338e06072967a1cb3eccf5f7794432a5b88d2
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-01-29 19:13:13 +0530

    Microsoft clearity added

--- src/app/industries/Intro.tsx
+++ src/app/industries/Intro.tsx
@@ -6,8 +6,8 @@ type IntroProps = {
 
 export default function Intro({ heading, text }: IntroProps) {
   return (
-    <section className="py-20 px-6 max-w-5xl mx-auto text-center">
-      <h2 className="text-3xl font-semibold">{heading}</h2>
+    <section className="py-20 px-6 max-w-9xl mx-auto text-center">
+      <h2 className="text-5xl  Text-black font-semibold">{heading}</h2>
       <p className="mt-6 text-gray-600 text-lg">{text}</p>
     </section>
   );
