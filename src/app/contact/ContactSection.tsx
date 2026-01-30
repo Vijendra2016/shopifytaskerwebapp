fe349dd6d42011a7629080328dcf667d3e8620ed
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-01-30 11:08:18 +0530

    Contact Page Added

--- src/app/contact/ContactSection.tsx
+++ src/app/contact/ContactSection.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import Image from "next/image";
 
 type TeamMember = {
   name: string;
   role: string;
   email: string;
-  image?: string;
+ 
 };
 
 const team: TeamMember[] = [
