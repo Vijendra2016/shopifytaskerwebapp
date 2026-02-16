f50d2ce2f2edb281b929894eeeaaff1d6851a8b2
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-02-16 10:54:52 +0530

    shopify store setup page setup

---
+++ src/app/shopify-store-setup/page.tsx
@@ -0,0 +1,16 @@
+import HeaderSection from "./HeaderSection";
+
+
+
+export default function MainPage() {
+  return (
+    
+    <section className="bg-[#eeeeee]">
+      <HeaderSection />
+  <div className="w-full mx-auto px-8">
+   
+  </div>
+</section>
+
+  );
+}
