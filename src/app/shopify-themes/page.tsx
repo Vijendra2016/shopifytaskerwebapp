c0e23d3c470fe88d7a3f4df8635f4a5dbdc94c0a
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-02-16 11:57:44 +0530

    shopify store setup page setup

--- src/app/shopify-themes/page.tsx
+++ src/app/shopify-themes/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function pricing() {
   return (
     <main className="container mx-auto px-4 py-10 text-center pt-20">
-        <h1 className="text-3xl md:text-6xl font-regular">ShopifyTasker Reviews</h1>
+        <h1 className="text-3xl md:text-6xl font-regular">We provide theme customization and update services for Shopify stores.</h1>
        <p className="text-xl font-light text-gray-900 mb-4">
        ShopifyTasker is highly praised for its reliable and efficient services, offering expert help with store setup, optimization, and marketing. Users appreciate its quick turnaround and excellent customer support
 
