4566615efd3d580dcb7359b8a10e77537e43bb6a
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-04-15 17:16:30 +0530

    added hair

--- src/app/industries/hair-extension-shopify-web-design/page.tsx
+++ src/app/industries/hair-extension-shopify-web-design/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Image from "next/image";
 import BreadcrumbSchema from "@/app/components/BreadcrumbSchema";
+import HeroSearchFirst from "./HeroBanner";
 
 import GeoMessage from "@/app/components/GeoMessage";
 export const metadata: Metadata = {
@@ -11,8 +12,17 @@ export const metadata: Metadata = {
 
 export default function pricing() {
   return (
-    <main className="container bg-[#fefdf9] mx-auto px-0 py-0 text-center pt-0">
+    
+    <main className=" bg-[#fefdf9] mx-auto px-0 py-0 text-center pt-0">
       <BreadcrumbSchema items={[{ name: "Home", href: "/" }, { name: "Industries", href: "/industries" }, { name: "Hair Extension Shopify Web Design", href: "/industries/hair-extension-shopify-web-design" }]} />
+     <HeroSearchFirst
+             videoWebmUrl="https://cdn.shopify.com/videos/c/o/v/1688ca4044654bb6b23b188f5bfd14a1.mp4"
+             videoUrl="https://cdn.shopify.com/videos/c/o/v/1688ca4044654bb6b23b188f5bfd14a1.mp4"
+             ctas={[
+               { label: "Get a Clear Estimate", variant: "primary" },
+               { label: "Explore Services", variant: "secondary" },
+             ]}
+           />
       <section className="relative flex items-center justify-center h-[60vh] md:h-[100vh] overflow-hidden">
         {/* Background image (optimized by Next.js) */}
         <Image
